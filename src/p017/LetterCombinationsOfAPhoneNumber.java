7c309a332db3847262450badf9dea9616ef743fe
lxsnum1 <672528307@qq.com>
2020-03-31 22:33:52 +0800

    problem 17 finished

--- src/p017/LetterCombinationsOfAPhoneNumber.java
+++ src/p017/LetterCombinationsOfAPhoneNumber.java
@@ -1,5 +1,9 @@
 package p017;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 //给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
 
 //
@@ -22,16 +26,92 @@ import java.util.List;
 public class LetterCombinationsOfAPhoneNumber {
 
     public static void main(String[] args) {
-        Solution solution = new LetterCombinationsOfAPhoneNumber().new Solution();
+        SolutionV2 solution = new LetterCombinationsOfAPhoneNumber().new SolutionV2();
+        solution.letterCombinations("23");
     }
 
-    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<String> letterCombinations(String digits) {
-            return null;
 
+            List<String> list = new ArrayList<>();
+            if (digits == null || digits.length() == 0) {
+                return list;
+            }
+            list.add("");
+            for (int i = 0; i < digits.length(); i++) {
+                list = combine(list, digits.charAt(i));
+            }
+            return list;
+        }
+
+        List<Character> numToCharList(char ch) {
+            switch (ch) {
+                case '2':
+                    return Arrays.asList('a', 'b', 'c');
+                case '3':
+                    return Arrays.asList('d', 'e', 'f');
+                case '4':
+                    return Arrays.asList('g', 'h', 'i');
+                case '5':
+                    return Arrays.asList('j', 'k', 'l');
+                case '6':
+                    return Arrays.asList('m', 'n', 'o');
+                case '7':
+                    return Arrays.asList('p', 'q', 'r', 's');
+                case '8':
+                    return Arrays.asList('t', 'u', 'v');
+                case '9':
+                    return Arrays.asList('w', 'x', 'y', 'z');
+                default:
+                    return Arrays.asList();
+            }
+        }
+
+        List<String> combine(List<String> ls, char ch) {
+            List<String> newList = new ArrayList<>();
+            for (String string : ls) {
+                for (char c : numToCharList(ch)) {
+                    newList.add(string + c);
+                }
+            }
+            return newList;
         }
     }
-    // leetcode submit region end(Prohibit modification and deletion)
 
+    class SolutionV2 {
+        private List<String> res;
+        private String letterMap[] = { "", // 0
+                "", // 1
+                "abc", // 2
+                "def", // 3
+                "ghi", // 4
+                "jkl", // 5
+                "mno", // 6
+                "pqrs", // 7
+                "tuv", // 8
+                "wxyz" // 9
+        };
+
+        public List<String> letterCombinations(String digits) {
+            res = new LinkedList<>();
+            if (digits == null || digits.equals("")) {
+                return res;
+            }
+
+            findCombination(digits, 0, "");
+            return res;
+        }
+
+        private void findCombination(String digits, int index, String s) {
+            if (index == digits.length()) {
+                res.add(s);
+                return;
+            }
+
+            String letter = letterMap[digits.charAt(index) - '0'];
+            for (int i = 0; i < letter.length(); i++) {
+                findCombination(digits, index + 1, s + letter.charAt(i));
+            }
+        }
+    }
 }
