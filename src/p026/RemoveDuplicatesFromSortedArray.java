5675c65338b1116dda8a166ff7db937e55db742a
lxsnum1 <672528307@qq.com>
2020-04-03 23:17:04 +0800

    problem 26 finished

--- src/p026/RemoveDuplicatesFromSortedArray.java
+++ src/p026/RemoveDuplicatesFromSortedArray.java
@@ -1,11 +1,9 @@
 package p026;
 
-//给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。 
+import java.util.Arrays;
 
-//
+// 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。 
 // 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。 
-//
-// 
 //
 // 示例 1: 
 //
@@ -34,11 +32,11 @@ package p026;
 //
 // 你可以想象内部操作如下: 
 //
-// // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
-//int len = removeDuplicates(nums);
+// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+// int len = removeDuplicates(nums);
 //
-//// 在函数里修改输入数组对于调用者是可见的。
-//// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+// 在函数里修改输入数组对于调用者是可见的。
+// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
 //for (int i = 0; i < len; i++) {
 //    print(nums[i]);
 //}
@@ -51,13 +49,20 @@ public class RemoveDuplicatesFromSortedArray {
         Solution solution = new RemoveDuplicatesFromSortedArray().new Solution();
     }
 
-    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int removeDuplicates(int[] nums) {
-            return 0;
+            if (nums == null) {
+                return 0;
+            }
 
+            int i = 0; // i 表示唯一元素的index
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[j] != nums[i]) {
+                    i += 1;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
         }
     }
-    // leetcode submit region end(Prohibit modification and deletion)
-
 }
