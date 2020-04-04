2177162be16f49c67d76bbcd01de23aff6ebcbd5
lxsnum1 <672528307@qq.com>
2020-04-04 11:36:21 +0800

    problem 27 finished

--- src/p027/RemoveElement.java
+++ src/p027/RemoveElement.java
@@ -1,8 +1,6 @@
 package p027;
 
 //给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。 
-
-//
 // 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。 
 //
 // 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。 
@@ -56,13 +54,33 @@ public class RemoveElement {
         Solution solution = new RemoveElement().new Solution();
     }
 
-    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int removeElement(int[] nums, int val) {
-            return val;
+            int left = 0, right = nums.length - 1;
+            while (left <= right) {
+                if (nums[left] == val) {
+                    nums[left] = nums[right];
+                    right -= 1;
+                } else {
+                    left += 1;
+                }
+            }
+            return right + 1;
+        }
 
+        public int removeElementV2(int[] nums, int val) {
+            if (nums == null) {
+                return 0;
+            }
+            int i = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != val) {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
         }
     }
-    // leetcode submit region end(Prohibit modification and deletion)
 
 }
