62e1603625deef214f53b959f435f74b16773dca
lxsnum1 <672528307@qq.com>
2020-06-02 15:47:41 +0800

    problem 81 finished

--- src/p081/SearchInRotatedSortedArrayIi.java
+++ src/p081/SearchInRotatedSortedArrayIi.java
@@ -1,6 +1,6 @@
 package p081;
 
-//假设按照升序排序的数组在预先未知的某个点上进行了旋转。 
+// 假设按照升序排序的数组在预先未知的某个点上进行了旋转。 
 //
 // ( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。 
 //
@@ -9,13 +9,13 @@ package p081;
 // 示例 1: 
 //
 // 输入: nums = [2,5,6,0,0,1,2], target = 0
-//输出: true
+// 输出: true
 // 
 //
 // 示例 2: 
 //
 // 输入: nums = [2,5,6,0,0,1,2], target = 3
-//输出: false 
+// 输出: false 
 //
 // 进阶: 
 //
@@ -29,12 +29,41 @@ public class SearchInRotatedSortedArrayIi {
 
     public static void main(String[] args) {
         Solution solution = new SearchInRotatedSortedArrayIi().new Solution();
+        int[] nums = new int[] { 0, 1, 1, 2, 0, 0 };
+        solution.search(nums, 2);
     }
 
     class Solution {
         public boolean search(int[] nums, int target) {
+            int lo = 0, hi = nums.length - 1;
+            while (lo <= hi) {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] == target) {
+                    return true;
+                }
 
+                // 去掉一个重复的干扰项，eg: nums= {1,0,1,1,1} or {1,1,1,0,1}
+                if (nums[lo] == nums[mid]) {
+                    lo++;
+                    continue;
+                }
+
+                if (nums[mid] > nums[lo]) {// 左半部分不含旋转
+                    if (target < nums[mid] && target >= nums[lo]) { // 目标在左半部分
+                        hi = mid - 1;
+                    } else {
+                        lo = mid + 1;
+                    }
+                } else {// 右半部分不含旋转
+                    if (target > nums[mid] && target <= nums[hi]) { // 目标在右半部分
+                        lo = mid + 1;
+                    } else {
+                        hi = mid - 1;
+                    }
+                }
+            }
+
+            return false;
         }
     }
-
 }
